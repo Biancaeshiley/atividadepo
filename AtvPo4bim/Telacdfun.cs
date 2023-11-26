6fffdda56fec9cf725c071419bfc3e6ca88a4afa
Bianca Eshiley <eshileybianca4@gmail.com>
2023-11-26 16:03:21 -0400

    primeiro commit

---
+++ AtvPo4bim/Telacdfun.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtvPo4bim
+{
+    public partial class Telacdfun : Form
+    {
+        public Telacdfun()
+        {
+            InitializeComponent();
+
+            voltarcdf.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            voltarcdf.FlatAppearance.BorderSize = 0;
+            voltarcdf.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            voltarcdf.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            voltarcdf.BackColor = Color.Transparent;
+
+            deletarf.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            deletarf.FlatAppearance.BorderSize = 0;
+            deletarf.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            deletarf.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            deletarf.BackColor = Color.Transparent;
+
+            cadastrarf.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            cadastrarf.FlatAppearance.BorderSize = 0;
+            cadastrarf.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            cadastrarf.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            cadastrarf.BackColor = Color.Transparent;
+        }
+
+        private void voltarcdf_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Telacdfun_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cepfun_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cadastrarf_Click(object sender, EventArgs e)
+        {
+            string id = IDfun.Text;
+            string nome = nomefun.Text;
+            string cpf = cpffun.Text;
+            string rg = rgfun.Text;
+            string telefone = telefonefun.Text;
+            string email = emailfun.Text;
+            string datanas = datanasfun.Text;
+            string endereco = endereçofun.Text;
+            double salario = Convert.ToDouble(salariofun.Text); 
+            string cidade = cidadefun.Text;
+            string estado = estadofun.Text;
+            string funcao = funcaofun.Text;
+            string cep = cepfun.Text;
+
+
+            Validacoes.ValidaCPF(cpf);
+            MessageBox.Show(Validacoes.ValidaCPF(cpf).ToString());
+
+
+        }
+    }
+}
---
+++ validacoesfun.cs
@@ -0,0 +1,101 @@
+﻿public class validacoesfun
+{
+    public string Id { get; set; }
+
+    public string Nome { get; set; }
+
+    public string Cpf { get; set; }
+
+    public string Rg { get; set; }
+
+    public string Telefone { get; set; }
+
+    public string Email { get; set; }
+
+    public string Datanasc { get; set; }
+
+    public string Endereco { get; set; }
+
+    public double Salario { get; set; }
+
+    public string Cidade { get; set; }
+
+    public string Estado { get; set; }
+
+    public string Funcao { get; set; }
+
+    public string Cep { get; set; }
+
+    public validacoesfun()
+    {
+
+    }
+
+    public validacoesfun(string id, string nome, string cpf, string rg, string email, string datanasc, string endereco, double salario,
+        string cidade, string estado, string funcao, string cep)
+    {
+        id = id;
+        nome = nome;
+        cpf = cpf;
+        rg = rg;
+        email = email;
+        datanasc = datanasc;
+        endereco = endereco;
+        salario = salario;
+        cidade = cidade;
+        estado = estado;
+        funcao = funcao;
+        cep = cep;
+    }
+
+  
+        public static bool ValidaCPF(string CPF)
+        {
+            CPF = CPF.Replace(".", "");
+            CPF = CPF.Replace("-", "");
+
+            if (CPF.Length != 11)
+            { return false; }
+
+            int s = 0;
+            int n1 = 10;
+            for (int i = 0; i < 9; i++)
+            {
+                s += Convert.ToInt32(CPF[i].ToString()) * n1;
+                n1--;
+            }
+
+            int res = s % 11;
+
+            if (res < 2)
+            { res = 0; }
+
+            else
+            { res = 11 - res; }
+
+            
+            int so = 0;
+            int n2 = 11;
+            for (int i = 0; i < 10; i++)
+            {
+                so += Convert.ToInt32(CPF[i].ToString()) * n2;
+                n2--;
+            }
+
+            int res2 = so % 11;
+
+            if (res2 < 2)
+            { res2 = 0; }
+
+            else
+            { res2 = 11 - (so % 11); }
+
+            if (res == Convert.ToInt32(CPF[9].ToString()) && res2 == Convert.ToInt32(CPF[10].ToString()))
+            { return true; }
+
+            else { return false; }
+            
+        }
+    
+}
+    
